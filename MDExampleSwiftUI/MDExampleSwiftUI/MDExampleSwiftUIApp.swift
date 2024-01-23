47e3f455efe7a74af21f95a545a4051847bef5e6
Carles Cañadas Torrents <>
2024-01-23 15:25:06 +0100

    feat: swiftui initial commit

---
+++ MDExampleSwiftUI/MDExampleSwiftUI/MDExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDExampleSwiftUIApp.swift
+//  MDExampleSwiftUI
+//
+//  Created by Carles Cañadas Torrents on 23/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MDExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
